9afce9ddc52ef764e98a65b74712e0583e829257
Abha Malviya <abhamalviya123@gmail.com>
2024-10-07 16:47:40 +0530

    committed the changes

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Test1 {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<>();
+        al.add("Abha");
+        al.add("Abc");
+        al.add("xyz");
+        System.out.println(al);
+    }
+}
