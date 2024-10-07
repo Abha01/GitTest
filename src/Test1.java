539def10ac86ecca308a5ca448505ee1cc08b895
Abha Malviya <abhamalviya123@gmail.com>
2024-10-07 16:52:29 +0530

    committed the changes

--- src/Test1.java
+++ src/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
     public static void main(String[] args) {
         ArrayList<String> al = new ArrayList<>();
         al.add("A");
-        al.add("Abc");
+        al.add("Abcd");
         al.add("xyz");
         System.out.println(al);
     }
